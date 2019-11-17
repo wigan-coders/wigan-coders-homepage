b3d20e30d8122d31c063f93b38b52e7ccaa5158f
Ryan Brockley <rybrockers@yahoo.com>
2019-11-17 19:35:27 +0000

    Almost finished Contact us styling

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -7,13 +7,30 @@
         <h3 class="contact-h3">Find us on Social Media</h3>
         <div class="icons">
           <a href="https://www.facebook.com/rybrockley/" target="blank">
-          <i class="fab fa-facebook"></i>
+            <i class="fab fa-facebook"></i>
           </a>
-          <a href="https://twitter.com/WiganCoders" target="blank">
-          <i class="fab fa-twitter"></i>
+          <a href="https://twitter.com/WiganCoders/" target="blank">
+            <i class="fab fa-twitter"></i>
           </a>
-          <a href="https://github.com/wigan-coders/wigan-coders-homepage" target="blank">
-          <i class="fab fa-github"></i>
+          <a href="https://github.com/wigan-coders/" target="blank">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="mid-content">
+      <h2 class="email-h2">Not on Social Media</h2>
+      <p>Send us an email or give us a call</p>
+
+      <div class="contact-links">
+        <h3 class="contact-h3">Email or telephone</h3>
+        <div class="icons-2">
+          <a href="#" target="blank">
+            <i class="fas fa-phone"></i>
+            <span class="phone-span">07539294710</span>
+          </a>
+          <a href="mailto:wigan.coders@gmail.com" target="blank">
+            <i class="far fa-envelope"></i>
           </a>
         </div>
       </div>
@@ -45,14 +62,39 @@ nav {
   flex-direction: column;
   margin-top: 4%;
 }
-
+.main-content p {
+  padding: 0;
+  margin: 0;
+}
+.mid-content p {
+  padding: 0 auto;
+  margin: 0 auto;
+}
+.mid-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 4%;
+}
 .contact-links {
   background: #e6e8e1;
   width: 100%;
-  max-width: 500px;
+  max-width: 600px;
   margin-top: 5%;
+  -webkit-box-shadow: 3px 4px 5px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 3px 4px 5px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 3px 4px 5px 0px rgba(0, 0, 0, 0.75);
+  padding: 5px;
+  margin-bottom: 15%;
 }
+
 .contact-h2 {
+  font-size: 40px;
+  padding: 0 auto;
+  margin: 0 auto;
+}
+.email-h2 {
   font-size: 40px;
   padding: 0 auto;
   margin: 0 auto;
@@ -62,6 +104,14 @@ nav {
 }
 
 .fab {
+  font-size: 50px;
+  padding: 10px;
+}
+.fas {
+  font-size: 50px;
+  padding: 10px;
+}
+.far {
   font-size: 50px;
   padding: 10px;
 }
@@ -76,6 +126,12 @@ nav {
 .fa-github {
   color: #000000;
 }
+.fa-phone {
+  color: #000000;
+}
+.fa-envelope {
+  color: #3b3b3b;
+}
 
 .icons {
   display: flex;
@@ -83,8 +139,14 @@ nav {
   align-items: center;
   flex-direction: row;
 }
-.main-content p {
-  padding: 0;
-  margin: 0;
+.icons-2 {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  color: #000000;
+}
+.icons-2 .phone-span {
+  color: #000000;
 }
 </style>
