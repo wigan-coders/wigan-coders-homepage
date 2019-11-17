36d7b92b2aab76f673be856918e6a4c9658253d8
Ryan Brockley <rybrockers@yahoo.com>
2019-11-17 16:44:29 +0000

    first push

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="navbar">
+  <nav class="grey darken-3">
+      <router-link :to="{name: 'Home' }" class="brand-logo left">Wigan Coders</router-link>
+      <ul class="right">
+        <li><router-link :to="{name: 'About' }">About</router-link></li>
+        <li><router-link :to="{name: 'Contact' }">Contact us</router-link></li>
+
+
+      </ul>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
